baa5eb7d8fff6199c2f769d51b76e93260221b1a
Dima Savenkov <savenkovdb@gmail.com>
2018-06-28 14:16:40 +0300

    Update envExample.js file

--- server/config/envExample.js
+++ server/config/envExample.js
@@ -3,7 +3,9 @@ const env = {
     DATABASE_HOST: '',
     DATABASE_USERNAME: '',
     DATABASE_PASSWORD: '',
-    DATABASE_DIALECT: ''
+    DATABASE_DIALECT: '',
+    AUTH0_DOMAIN: '',
+    AUTH0_AUDIENCE: ''
 };
 
 module.exports = env;
