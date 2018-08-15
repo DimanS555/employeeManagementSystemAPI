77d241c31232699ad0de59e01e814874732ab25b
Dima Savenkov <savenkovdb@gmail.com>
2018-08-15 23:33:11 +0300

    Create tables with mock data on every startup

---
+++ server/mockData/mockDepartments.js
@@ -0,0 +1,14 @@
+module.exports = MOCK_DEPARTMENT = [
+    { 
+        id: 1, 
+        depName: 'Finance' 
+    },
+    { 
+        id: 2, 
+        depName: 'HR' 
+    },
+    { 
+        id: 3, 
+        depName: 'Tech' 
+    }
+];
---
+++ server/mockData/mockEmployees.js
@@ -0,0 +1,37 @@
+module.exports = MOCK_EMPLOYEE = [
+    { 
+        id: 1, 
+        firstName: "Dima", 
+        lastName: "Savenkov", 
+        isActive: true, 
+        emp_depID: 1 
+    },
+    { 
+        id: 2, 
+        firstName: "Peter", 
+        lastName: "Sermon", 
+        isActive: false, 
+        emp_depID: 2 
+    },
+    { 
+        id: 3, 
+        firstName: "Den", 
+        lastName: "Brown", 
+        isActive: false, 
+        emp_depID: 3 
+    },
+    { 
+        id: 4, 
+        firstName: "Kate", 
+        lastName: "Belik", 
+        isActive: true, 
+        emp_depID: 1 
+    },
+    { 
+        id: 5, 
+        firstName: "John", 
+        lastName: "Doe", 
+        isActive: true, 
+        emp_depID: 2 
+    },
+];
